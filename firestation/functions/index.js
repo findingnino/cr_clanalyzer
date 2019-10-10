107a3a303e648f817b4d1c14035a861ffb23316a
ngancitano <ngancitano@gmail.com>
2019-10-09 22:58:56 -0400

    firebase update

--- firestation/functions/index.js
+++ firestation/functions/index.js
@@ -1,8 +1,76 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const fetch = require("cross-fetch");
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    const snapshot = await admin.database().ref('/messages').push({original: original});
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref.toString());
+  });
+
+exports.findIPaddress = functions.https.onRequest(async (req, res) => {
+    fetch("http://httpbin.org/ip")
+    .then(result => result.json())
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
+});
+
+
+//eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQxOWFhMjY1LWJjZDMtNDhhZS05ZjFhLWY1OWFmNDEyZTliZSIsImlhdCI6MTU3MDU3ODM5NCwic3ViIjoiZGV2ZWxvcGVyL2Y3OTQ0YTA1LTg5YzQtNDdhMS0xMzdjLWM3NGRjMDA2MTU2YSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMDcuMTc4LjIzMS4yNDQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pq91LlBJd7GmRMfMetyeZ5CyWaYYZOdfxeHBxaedOrYCnm4Ja2l8PciCtCPmPhJfo44ImD8KS0CThG9N1mk9Fw
+exports.clanalyzertesting = functions.https.onRequest(async (req, res) => {
+    const url = "https://api.clashroyale.com/v1/clans/%2388QY8QQV/warlog"
+    const key = req.query.key;
+
+    let headers = {
+        'Accept': 'application/json',
+        'authorization': 'Bearer ' + key,
+    };
+
+    fetch(url, {
+        method: 'Get',
+        headers: headers,
+    }).then(result => result.json())
+      .then(result => res.send(JSON.stringify(result)))
+      .catch(err => res.send(err))
+
+});
+
+//eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQxOWFhMjY1LWJjZDMtNDhhZS05ZjFhLWY1OWFmNDEyZTliZSIsImlhdCI6MTU3MDU3ODM5NCwic3ViIjoiZGV2ZWxvcGVyL2Y3OTQ0YTA1LTg5YzQtNDdhMS0xMzdjLWM3NGRjMDA2MTU2YSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMDcuMTc4LjIzMS4yNDQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pq91LlBJd7GmRMfMetyeZ5CyWaYYZOdfxeHBxaedOrYCnm4Ja2l8PciCtCPmPhJfo44ImD8KS0CThG9N1mk9Fw
+exports.clanalyzertestingwithenvvar = functions.https.onRequest(async (req, res) => {
+    const url = "https://api.clashroyale.com/v1/clans/%2388QY8QQV/warlog"
+    const key = functions.config().clashroyale.key;
+
+    let headers = {
+        'Accept': 'application/json',
+        'authorization': 'Bearer ' + key,
+    };
+
+    fetch(url, {
+        method: 'Get',
+        headers: headers,
+    }).then(result => result.json())
+      .then(result => res.send(JSON.stringify(result)))
+      .catch(err => res.send(err))
+
+});
+
+exports.printclashkey = functions.https.onRequest((req, res) => {
+    
+    const wildcard = functions.config().clashroyale.key;
+    
+    res.send("Hello World");
+    res.send(wildcard);
+});
